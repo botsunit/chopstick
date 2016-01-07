cef0630b7f188540e1d22b62484ef625cbbcf552
sulphur <piotr.kedziora27@gmail.com>
2016-01-07 23:37:34 +0100

    New release 0.0.2

--- templates/command/new/elixir/lib/rest_controllers/my_rest_resources_controller.ex
+++ templates/command/new/elixir/lib/rest_controllers/my_rest_resources_controller.ex
@@ -1,5 +1,5 @@
 defmodule <%= project_name_camelize %>.RestControllers.MyRestResourcesController do
-  def show(_req, state) do
+  def show(req, state) do
     id = :cowboy_req.binding(:id, req)
     message = "Hello <%= project_name_camelize %>.RestControllers.MyRestResourcesController " <> id
     {200, [{"content-type", "text/plain"}], message, state}
